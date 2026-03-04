310710a924c466d2966b45915b32cad9879b705d
Brice WeKeys <formation@wekeys.fr>
2026-03-04 19:37:38 +0100

    Ajout du .env (caché) et début de BDD pour l'utilisateur

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const moment = require('moment')
+require('./models/connection');
+
 
 var indexRouter = require('./routes/index');
 var tripsRouter = require('./routes/trips');
