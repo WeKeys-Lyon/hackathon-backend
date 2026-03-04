a24965b31189af3e60e4f81047592742b2a1b7ac
Brice WeKeys <formation@wekeys.fr>
2026-03-04 19:37:20 +0100

    Ajout du .env (caché) et début de BDD pour l'utilisateur

---
+++ models/users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const cartSchema = mongoose.Schema({
+    departure: String,
+    arrival: String,
+    date: Date,
+    price: Number
+});
+const bookingSchema = mongoose.Schema({
+    departure: String,
+    arrival: String,
+    date: Date,
+    price: Number
+})
+const userSchema = mongoose.Schema({
+    cookie: Number,
+    cart: cartSchema,
+    booking: bookingSchema,
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
